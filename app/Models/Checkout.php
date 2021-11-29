6f8a4e1152b6617c226243980b09a93b2fc94a06
zofisultan <zofinofriandi23@gmail.com>
2021-11-29 17:27:58 +0700

    commit finish migration

---
+++ app/Models/Checkout.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Checkout extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var string[]
+     */
+    protected $fillable = [
+        'user_id',
+        'camp_id',
+        'card_number',
+        'expired',
+        'cvc',
+        'is_paid',
+    ];
+}
